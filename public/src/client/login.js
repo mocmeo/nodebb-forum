4895042c09721731c7310ed83a7e71e284436816
lamngo <nglam96@hotmail.com>
2018-11-03 15:12:42 +0700

    Update login function

--- public/src/client/login.js
+++ public/src/client/login.js
@@ -1,28 +1,29 @@
-'use strict';
+"use strict";
 
+define("forum/login", [], function() {
+	var Login = {};
 
-define('forum/login', [], function () {
-	var	Login = {};
+	Login.init = function() {
+		var errorEl = $("#login-error-notify");
+		var submitEl = $("#login");
+		var formEl = $("#login-form");
 
-	Login.init = function () {
-		var errorEl = $('#login-error-notify');
-		var submitEl = $('#login');
-		var formEl = $('#login-form');
-
-		submitEl.on('click', function (e) {
+		submitEl.on("click", function(e) {
 			e.preventDefault();
 
-			if (!$('#username').val() || !$('#password').val()) {
-				errorEl.find('p').translateText('[[error:invalid-username-or-password]]');
+			if (!$("#username").val() || !$("#password").val()) {
+				errorEl
+					.find("p")
+					.translateText("[[error:invalid-username-or-password]]");
 				errorEl.show();
 			} else {
 				errorEl.hide();
 
-				if (submitEl.hasClass('disabled')) {
+				if (submitEl.hasClass("disabled")) {
 					return;
 				}
 
-				submitEl.addClass('disabled');
+				submitEl.addClass("disabled");
 
 				/*
 					Set session refresh flag (otherwise the session check will trip and throw invalid session modal)
@@ -31,50 +32,93 @@ define('forum/login', [], function () {
 				app.flags = app.flags || {};
 				app.flags._sessionRefresh = true;
 
-				formEl.ajaxSubmit({
-					headers: {
-						'x-csrf-token': config.csrf_token,
-					},
-					success: function (returnTo) {
-						var pathname = utils.urlToLocation(returnTo).pathname;
+				var displayError = function(responseText) {
+					errorEl.find("p").translateText(responseText);
+					errorEl.show();
+					submitEl.removeClass("disabled");
+					app.flags._sessionRefresh = false;
 
-						var params = utils.params({ url: returnTo });
-						params.loggedin = true;
-						var qs = decodeURIComponent($.param(params));
+					// Select the entire password if that field has focus
+					if ($("#password:focus").length) {
+						$("#password").select();
+					}
+				};
 
-						window.location.href = pathname + '?' + qs;
+				var loginForum = function() {
+					formEl.ajaxSubmit({
+						headers: {
+							"x-csrf-token": config.csrf_token
+						},
+						beforeSubmit: function(data, $form, opts) {
+							// set password data without displaying
+							data[1].value = "forum@1234";
+						},
+						success: function(returnTo) {
+							var pathname = utils.urlToLocation(returnTo).pathname;
+
+							var params = utils.params({ url: returnTo });
+							params.loggedin = true;
+							var qs = decodeURIComponent($.param(params));
+
+							window.location.href = pathname + "?" + qs;
+						},
+						error: function(data) {
+              if (data.status === 403 && data.responseText === 'Forbidden') {
+							  window.location.href = config.relative_path + '/login?error=csrf-invalid';
+							} else {
+                displayError(data.responseText);
+              }
+						}
+					});
+				};
+
+				// Authenticate with vlms server
+				$.ajax({
+					url: "http://vlms.local/user/login?referrer=forum",
+					type: "POST",
+					dataType: "json",
+					data: {
+						lname: $("#username").val(),
+						pass: $("#password").val(),
+						submit: "1",
+						_sand_ajax: 1,
+						_sand_platform: 3,
+						_sand_readmin: 1,
+						_sand_is_wan: false,
+						_sand_domain: "ve"
 					},
-					error: function (data) {
-						if (data.status === 403 && data.responseText === 'Forbidden') {
-							window.location.href = config.relative_path + '/login?error=csrf-invalid';
+					headers: {},
+					crossDomain: true,
+					success: function(data, textStatus, xhr) {
+						if (data.success == true) {
+							loginForum();
 						} else {
-							errorEl.find('p').translateText(data.responseText);
-							errorEl.show();
-							submitEl.removeClass('disabled');
-							app.flags._sessionRefresh = false;
-
-							// Select the entire password if that field has focus
-							if ($('#password:focus').length) {
-								$('#password').select();
-							}
+							// Login failed
+              var responseText = "[[error:invalid-login-credentials]]";
+              displayError(responseText);
 						}
 					},
+					error: function(xhr, textStatus, errorThrown) {
+						console.log(errorThrown);
+					}
 				});
 			}
 		});
 
-		$('#login-error-notify button').on('click', function (e) {
+		$("#login-error-notify button").on("click", function(e) {
 			e.preventDefault();
 			errorEl.hide();
 			return false;
 		});
 
-		if ($('#content #username').attr('readonly')) {
-			$('#content #password').val('').focus();
+		if ($("#content #username").attr("readonly")) {
+			$("#content #password")
+				.val("")
+				.focus();
 		} else {
-			$('#content #username').focus();
+			$("#content #username").focus();
 		}
-		$('#content #noscript').val('false');
+		$("#content #noscript").val("false");
 	};
 
 	return Login;
